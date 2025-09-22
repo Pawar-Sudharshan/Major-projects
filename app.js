3b3a2e9f0aee02bbc9ab8024beb00b87d2a433d9
PAWAR SUDHARSHAN <pawarsudharshan47@gmail.com>
2025-09-22 12:13:37 +0000

    Fix MongoDB connection logic: moved connect to app.js, corrected schema export, configured dotenv.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 const mongoose = require("mongoose")
 
-
+MONGO_URL = "mongodb+srv://thikkalboys_db_user:7xCd4ozaYzz4dMFT@cluster0.tyigaei.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 main()
 .then(()=>{
     console.log("Connected to DB")
@@ -10,12 +10,16 @@ main()
 .catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/test');
+  await mongoose.connect(MONGO_URL)
+  .then(() => console.log('Connected to MongoDB Atlas'))
+  .catch((err) => console.error(err));;
 }
 
 app.get("/" , (req ,res)=>{
     res.send("rout is working.")
 })
+
+app.get("/")
 
 app.listen(8080 , ()=>{
     console.log("server is running.")
--- models/listings.js
+++ models/listings.js
@@ -1,29 +1,38 @@
-const mongoose = require("mongoose")
-const schema = mongoose.schema()
+const mongoose = require('mongoose');
+const dotenv = require("dotenv");
+dotenv.config();
+console.log("Mongo URI is: ", process.env.MONGO_URL);
 
-const listings = new schema({
-    title :{
-        type : String,
-        require : true
-    },
-    des : {
-        type : String
-    },
-    img : {
-        type: String
-    },
-    price:{
-        type:Number,
-        Range : (1000 , 10000)
-    },
-    state : {
-        type : String
-    },
-    country:{
-        type:String
-    }
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => console.log('Connected to MongoDB Atlas'))
+  .catch((err) => console.error(err));
 
-})
+// Define the schema
+const listingsSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  des: {
+    type: String
+  },
+  img: {
+    type: String,
+   set : (v) => v === ""?"https://unsplash.com/photos/the-sun-is-setting-over-the-mountains-in-the-distance-RsCvxI9h2Ew":v
+  },
+  price: {
+    type: Number,
+    min: 1000,
+    max: 10000
+  },
+  state: {
+    type: String
+  },
+  country: {
+    type: String
+  }
+});
 
-const listing = mongoose.model(listings , schema);
-module.export = listing;
+// Export the model
+const Listing = mongoose.model('Listing', listingsSchema);
+module.exports = Listing;
